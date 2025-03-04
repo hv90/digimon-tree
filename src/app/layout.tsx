e00d3b85d5f815d174f19e0f84f4394de4eefc00
hv90 <hugomachado182@gmail.com>
2025-03-04 15:54:55 -0300

    improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/assets/css/globals.css";
 import React from "react";
